f77f31905ea90063de265c571d766b4cedf9d5ea
Rhawan Brenner <rb.brenner10@gmail.com>
2016-12-12 00:07:29 -0200

    atvd #1 Criação do projeto inicial utilizando Spring Boot

---
+++ lab-cliente/src/main/java/br/alfa/labcliente/App.java
@@ -0,0 +1,13 @@
+package br.alfa.labcliente;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
