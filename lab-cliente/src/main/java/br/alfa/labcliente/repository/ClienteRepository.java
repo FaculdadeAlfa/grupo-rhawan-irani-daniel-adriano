5b863569dae381268b618773d2699d3c6106c25f
Daniel Filho <Daniel Filho@DanielFilho-PC>
2016-12-30 19:21:31 -0200

    Cria alguns testes unitários

--- lab-cliente/src/main/java/br/alfa/labcliente/repository/ClienteRepository.java
+++ lab-cliente/src/main/java/br/alfa/labcliente/repository/ClienteRepository.java
@@ -3,9 +3,14 @@ package br.alfa.labcliente.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.alfa.labcliente.entity.Cliente;
+import br.alfa.labcliente.entity.Endereco;
+import br.alfa.labcliente.entity.Telefone;
 
 public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 	
 	Cliente findByNome(String nome);
+	Cliente findByCpfCnpj(String cpfCnpj);
+	Cliente findByEndereco(Endereco endereco);
+	Cliente findByTelefone(Telefone telefone);
 	
 }
--- lab-cliente/src/test/java/br/alfa/labcliente/repository/ClienteRepositoryTest.java
+++ lab-cliente/src/test/java/br/alfa/labcliente/repository/ClienteRepositoryTest.java
@@ -9,6 +9,8 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import br.alfa.labcliente.entity.Cliente;
+import br.alfa.labcliente.entity.Endereco;
+import br.alfa.labcliente.entity.Telefone;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
@@ -31,5 +33,58 @@ public class ClienteRepositoryTest {
 		
 		assertEquals("12345678901", clienteConsulta.getCpfCnpj());
 	}
+	
+	@Test
+	public void findByCpfCnpj() {
+		Cliente cliente = new Cliente();
+		cliente.setNome("Joao Marcos");
+		cliente.setCpfCnpj("12345678901");
+		this.entityManager.persist(cliente);
+		
+		Cliente clienteConsulta = this.repository.findByCpfCnpj("12345678901");
+		
+		assertEquals("Joao Marcos", clienteConsulta.getNome());
+	}
+	
+	@Test
+	public void findByEndereco() {
+		
+		Endereco endereco = new Endereco();
+		
+		endereco.setBairro("St. Oeste");
+		endereco.setLogradouro("logradouro");
+		endereco.setNumero("41");
+		endereco.setCidade("Goiania");
+		endereco.setCep("74000000");
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("Pedro Paulo");
+		cliente.setCpfCnpj("12345678901");
+		cliente.setEndereco(endereco);
+		
+		this.entityManager.persist(cliente);
+		
+		Cliente clienteConsulta = this.repository.findByEndereco(endereco);
+		
+		assertEquals("Pedro Paulo", clienteConsulta.getNome());
+	}
 
+	@Test
+	public void findByTelefone() {
+		
+		Telefone telefone = new Telefone();
+		telefone.setFixo("33333022");
+		
+		Cliente cliente = new Cliente();
+		cliente.setNome("Joao Marcos");
+		cliente.setCpfCnpj("12345678901");
+		cliente.setTelefone(telefone);
+		
+		this.entityManager.persist(cliente);
+		
+		Cliente clienteConsulta = this.repository.findByTelefone(telefone);
+		
+		assertEquals("Joao Marcos", clienteConsulta.getNome());
+	}
+	
 }
